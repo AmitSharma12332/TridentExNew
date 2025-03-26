4ccd782a7aca62d18823e934a8b947c0a99a2dc5
AmitSharma12332 <amitsharma96696@gmail.com>
2025-03-26 20:32:13 +0530

    Update Bookmaker.jsx in bookmaker name

--- src/components/matchdetails_ui/Bookmaker.jsx
+++ src/components/matchdetails_ui/Bookmaker.jsx
@@ -206,10 +206,11 @@ const BookmakerComponent = ({
 
   const bookmakerMarket = data.find(
     (market) =>
-      (market.market?.name === "Bookmaker" || market.market?.name === "Bookmaker 0%Comm") &&
+      (market.market?.name.toLowerCase().trim() === "bookmaker" ||
+        market.market?.name.toLowerCase().trim() === "bookmaker 0% comm") &&
       Array.isArray(market.odds?.runners) &&
-      market.odds.runners.length > 0,
-  )
+      market.odds.runners.length > 0
+  );
 
   const getMargins = useCallback(
     async (token) => {
