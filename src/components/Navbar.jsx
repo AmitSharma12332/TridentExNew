d2750f2517c663a9747ada4a5d067b9b28398d23
AmitSharma12332 <amitsharma96696@gmail.com>
2025-06-25 16:06:28 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -261,9 +261,9 @@ const NavbarComponent = ({ toggleSidebar, showsidebar }) => {
         {/* Marquee for important announcements */}
         <div className="marquee flex bg-red-600 text-white py-1 px-2 rounded-md mb-2 overflow-hidden">
           <div className="animate-marquee whitespace-nowrap text-sm font-medium">
-            🚨 IMPORTANT: Event ID 34440606 - Bangladesh vs Sri Lanka match bets will
-            be canceled due to odds glitch. All bets will be canceled and
-            amounts will be refunded. 🚨
+            🚨 IMPORTANT: Event IDs 34440606 & 34448947 - Bangladesh vs Sri Lanka and West Indies v Australia
+            match bets will be canceled due to odds glitch. All bets will be
+            canceled and amounts will be refunded. 🚨
           </div>
         </div>
 
